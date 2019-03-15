094b60b6da890b526c9db433cc918f66550c88ba
Vinicius Barreira <viniciustes@gmail.com>
2019-03-14 21:21:10 -0300

    Possible moves of a piece

--- Eclipse-workaspace/Aula124_Projeto_Xadrez/src/boardgame/Board.java
+++ Eclipse-workaspace/Aula124_Projeto_Xadrez/src/boardgame/Board.java
@@ -54,11 +54,24 @@ public class Board {
 		return positionExists(position.getRow(), position.getColumn());
 	}
 
-	public boolean thereIsAPiece(Position position) {
+	public Piece removePiece(Position position) {
 		if (!positionExists(position)) {
 			throw new BoardException("Position not on the board");
 		}
 
+		if (piece(position) == null) {
+			return null;
+		}
+		Piece aux = piece(position);
+		aux.position = null;
+		pieces[position.getRow()][position.getColumn()] = null;
+		return aux;
+	}
+
+	public boolean thereIsAPiece(Position position) {
+		if (!positionExists(position)) {
+			throw new BoardException("Position not on the board");
+		}
 		return piece(position) != null;
 	}
 
--- Eclipse-workaspace/Aula124_Projeto_Xadrez/src/boardgame/Piece.java
+++ Eclipse-workaspace/Aula124_Projeto_Xadrez/src/boardgame/Piece.java
@@ -1,17 +1,35 @@
 package boardgame;
 
-public class Piece {
+public abstract class Piece {
 
 	protected Position position;
 	private Board board;
 
 	public Piece(Board board) {
-
 		this.board = board;
+		position = null;
+	}
+	
+	protected Board getBoard() {
+		return board;
 	}
 
-	protected Position getPosition() {
+	public abstract boolean[][] possibleMoves();
 
-		return position;
+	public boolean possibleMove(Position position) {
+		return possibleMoves()[position.getRow()][position.getColumn()];
+	}
+
+	public boolean isThereAnyPossibleMove() {
+		boolean[][] mat = possibleMoves();
+
+		for (int i = 0; i < mat.length; i++) {
+			for (int j = 0; j < mat.length; j++) {
+				if (mat[i][j]) {
+					return true;
+				}
+			}
+		}
+		return false;
 	}
 }
